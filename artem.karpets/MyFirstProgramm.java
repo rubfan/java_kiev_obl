d2707e8dca9a0647f8d096a67b57c4908d8d65cb
ArtKarpets <Artkarpets@gmail.com>
2020-12-20 14:16:21 +0200

    Add my weight

--- artem.karpets/MyFirstProgramm.java
+++ artem.karpets/MyFirstProgramm.java
@@ -2,5 +2,6 @@ class MyFirstProgramm{
     public static void main(String[]args){
         System.out.println("MyNameIs "+args[0]);
         System.out.println("My age is "+args[1]);
+        System.out.println("My weight "+args[2]);
     }
 }
