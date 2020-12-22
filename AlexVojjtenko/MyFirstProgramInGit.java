1b3a1ae897b51e05397f31336d5ccfed66922fbd
Alexandr Vojtenko <alexandrvojjtenko@gmail.com>
2020-12-23 01:11:33 +0200

    clean all data

--- AlexVojjtenko/MyFirstProgramInGit.java
+++
@@ -1,6 +0,0 @@
-public class MyFirstProgramInGit {
-     public static void main(String[] args) {
-           System.out.println("My name is "+args[0]);
-		   System.out.println("My age is "+ args[1]);
-  }
-} 
