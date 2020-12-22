76a2d6bfaad6ddab5a2b28883afc6a0d3257530b
AsapUser <denis133713372@gmail.com>
2020-12-23 05:05:00 +0800

    Added homework1 for Ruslan Gramatic) (mk-2)

--- suk.denys/MyFirstProgram.java
+++ suk.denys/MyFirstProgram.java
@@ -1,6 +1,5 @@
 class MyFirstProgram{
  public static void main(String[] args) {
-    System.out.print("Hello " + args[0]);
-    System.out.print(". My age is " + args[1]);
+    System.out.print("Hello " + args[0] + " " + args[1] + "!");
   }
 }
