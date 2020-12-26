4f7d42dcccfb8cf4fac0acb23a80acde80cbd765
Nikita <nikita.filianin94@gmail.com>
2020-12-26 14:58:44 +0200

    Refacored Program and Info file

--- nikita.filianin/MyFirstProgram.java
+++ nikita.filianin/MyFirstProgram.java
@@ -1,7 +1,5 @@
 class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("My name is "+args[0]);
-		System.out.println("My surname is "+args[1]);
-		System.out.println("Hello Ruslan Gramatic!");
+		System.out.println("Hello "+args[0]+" "+args[1]);
 	}
 }
