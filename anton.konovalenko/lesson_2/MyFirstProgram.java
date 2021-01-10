f1e648393c50187f8b22ff07a863ef568b45c6e7
Anton <49767210+antnknvlnk@users.noreply.github.com>
2021-01-10 13:32:27 +0200

    Hw 3 (#19)
    * Homework 1
    
    * Added test.txt
    
    * Added start.txt
    
    * Added test2.txt
    
    * Added test2.txt with ccc
    
    * added test file

---
+++ anton.konovalenko/lesson_2/MyFirstProgram.java
@@ -0,0 +1,5 @@
+class MyFirstProgram {
+	public static void main(String[] args){
+		System.out.println("Hello, "+args[0]);
+		System.out.println("My age is "+args[1]);
+	}}
---
+++ anton.konovalenko/lesson_2/src/Hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Hello {
+        public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Type here your name: ");
+		String name = scanner.nextLine();
+
+		System.out.println("Type here your surname: ");
+                String surname = scanner.nextLine();
+
+		System.out.println("Hello, " + name + " " + surname + "!");
+		scanner.close();
+        }
+
+}
+
